13323c3cd4d05df2448836bf7ca867d51baabcdf
Stefan Vlahov <stefan.vlahov5@gmail.com>
2016-04-03 19:25:34 -0500

    Add game over method and tests

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,6 +1,6 @@
 module TicTacToe
   class Board
-    attr_reader :grid 
+    attr_reader :grid
     def initialize(input = {})
       @grid = input.fetch(:grid, default_grid)
     end
@@ -11,6 +11,12 @@ module TicTacToe
 
     def set_cell(x, y, value)
       get_cell(x, y).value = value
+    end
+
+    def game_over
+      return :winner if winner?
+      return :draw if draw?
+      false
     end
 
     private
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,5 +46,27 @@ module TicTacToe
       end
     end
 
+    context "#game_over" do
+      it "returns :winner if winner? is true" do
+        board = Board.new
+        board.stub(:winner?) { true }
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :draw if winner? is false and draw? is true" do
+        board = Board.new
+        board.stub(:winner?) { false }
+        board.stub(:draw?) { true }
+        expect(board.game_over).to eq :draw
+      end
+
+      it "returns false if winner? is false and draw? is false" do
+        board = Board.new
+        board.stub(:winner?) { false }
+        board.stub(:draw?) { false }
+        expect(board.game_over).to be false
+      end
+    end
+
   end
 end
